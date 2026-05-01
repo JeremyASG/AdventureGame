803cb4ce06e11b53ca6bd52bbea7cdf4bfe32f7f
JeremyASG <elhombrexdxd@gmail.com>
2026-04-30 20:03:18 -0400

    Add: AdventureGame.cs

--- src/AdventureGame/Program.cs
+++ src/AdventureGame/Program.cs
@@ -4,13 +4,7 @@ public class Program
 {
 	public static void Main()
 	{
-		Adventurer a = new Adventurer();
-        
-		Adventurer b = new Adventurer();
-		b.SetLamp(true);
-		b.SetKey(true);
-	
-		Console.WriteLine(a);
-		Console.WriteLine(b);
+        var game = new AdventureGame();
+		game.Start();
 	}
 }
